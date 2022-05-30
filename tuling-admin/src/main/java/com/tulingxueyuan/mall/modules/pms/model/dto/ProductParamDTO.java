306fbeadd4f90df0cda96d0e8e05a6b4c56b92d4
Administrator <1013560420@qq.com>
2022-05-30 15:45:34 +0800

    封装后台商品列表查询参数的DTO

---
+++ tuling-admin/src/main/java/com/tulingxueyuan/mall/modules/pms/model/dto/ProductParamDTO.java
@@ -0,0 +1,33 @@
+package com.tulingxueyuan.mall.modules.pms.model.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.models.auth.In;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * {
+ *     keyword: null,
+ *     pageNum: 1,
+ *     pageSize: 5,
+ *     publishStatus: null,
+ *     verifyStatus: null,
+ *     productSn: null,
+ *     productCategoryId: null,
+ *     brandId: null
+ *   }
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value = "商品列表查询参数DTO", description = "用于商品列表查询参数接收")
+public class ProductParamDTO {
+
+    private String keyword;
+    private Integer pageNum;
+    private Integer pageSize;
+    private Integer publishStatus;
+    private Integer veryfyStatus;
+    private String productSn;
+    private Long productCategoryId;
+    private Long brandId;
+}
